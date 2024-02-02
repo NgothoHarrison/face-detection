347ad9010d43a25311b16ad1cc8bd8168c1a28f9
Harrison Hank <codinghank@gmail.com>
2024-02-02 23:09:16 +0300

    modified:   faces_train.py

--- faces_train.py
+++ faces_train.py
@@ -1,5 +1,5 @@
 import os
-import cv2
+import cv2 as cv
 import numpy as np
 
 # People's names
@@ -17,7 +17,21 @@ features = []
 lables = []
 
 def train():
-    
+    for person in people:
+        path = os.path.join(DIR, person)
+        label = people.index(person)
+
+        for img in os.listdir(path):
+            img_path = os.path.join(path,img)
+
+            img_array = cv.imread(img_path)
+            gray = cv.cvtColor(img_array, cv.COLOR_BG2BGR)
+
+            
+
+
+
+
 
 
 
